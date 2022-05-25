28d4d2c3a571f1770018554576af7cd158a72597
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-25 18:17:39 +0530

    update

--- jupiter/src/App.js
+++ jupiter/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-import User from './User.js';
+import Header from './header';
 
 
 
 function App() {
   return (
     <div className="App">
-      <User></User>
+     <Header></Header>
+     
     
     </div>
   );
