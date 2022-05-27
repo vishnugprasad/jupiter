6cb03b4856fb6d4ad0d5dcf0fb9edfbcd1b94075
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-27 19:21:18 +0530

    useref update

--- jupiter/src/App.js
+++ jupiter/src/App.js
@@ -1,15 +1,23 @@
 
 import './App.css';
-import Header from './header';
+import {useEffect, useState, useRef} from "react"
 
 
 
 function App() {
+ 
+ 
+  const[name, setName] = useState("")
+  const renderCount = useRef(0);
+
+  useEffect(()=>{
+    renderCount.current = renderCount.current+1
+  })  
   return (
     <div className="App">
-     <Header></Header>
-     
-    
+      <input type = "text" value ={name} onChange ={e => setName(e.target.value)}></input>
+      <div> my name is {name}</div>
+      <div>i rendered{renderCount.current} times</div>
     </div>
   );
 }
---
+++ jupiter/src/useeffect.js
@@ -0,0 +1,33 @@
+// import React, { useReduce} from 'react'
+
+// function reducer(state, action){
+//   return { count : state.count + 1}
+  
+// }
+
+// function Hello() {
+  
+    
+//     const [state,dispatch] = useReduce(reducer, {count:0})
+//     // const[count,setCount] = useState(0  )
+//     function increment(){
+
+//       dispatch()
+//     }
+
+//     function decrement(){
+//       // setCount(prevCount => prevCount-1)
+
+//     }
+    
+//   return (
+//     <div>
+//       <button onClick = {decrement}>-</button>
+//       <span>{state.count}</span>
+//       <button onClick = {increment}>+</button>
+//     </div>
+   
+//   )
+// }
+
+// export default Hello
